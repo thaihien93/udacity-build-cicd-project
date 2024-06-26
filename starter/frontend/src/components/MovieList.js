1fa017e517dfb13014f85af4db86e77d3b2d86fd
HienT4 <thaihien121293@gmail.com>
2024-06-26 03:49:22 +0000

    debug new 3

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
+  console.log('REACT_APP_MOVIE_API_URL', process.env.REACT_APP_MOVIE_API_URL);
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
       setMovies(response.data.movies);
